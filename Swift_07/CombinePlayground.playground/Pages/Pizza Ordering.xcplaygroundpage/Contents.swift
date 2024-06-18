913007935815737a82ee1b57a44a3281950c622a
hyerim <ax1021@naver.com>
2024-06-18 14:30:25 +0900

    Pizza Ordering 내용 추가

--- Swift_07/CombinePlayground.playground/Pages/Pizza Ordering.xcplaygroundpage/Contents.swift
+++ Swift_07/CombinePlayground.playground/Pages/Pizza Ordering.xcplaygroundpage/Contents.swift
@@ -1,14 +1,19 @@
 import Foundation
 import Combine
 
-let pizzaOrder = Order()
+let pizzaOrder = Order() // 주문 상태를 저장하는 객체
 
+// 만약 pizzaOrderPublisher 게시가 되면 어떻게 동작할 것인지에 대한 설계 -----------------------
 let pizzaOrderPublisher = NotificationCenter.default.publisher(for: .didUpdateOrderStatus,
                                                                object: pizzaOrder)
 
 pizzaOrderPublisher.sink { notification in
-    print(notification)
-    dump(notification)
+    Task {
+        try? await Task.sleep(for: .seconds(2))
+        print("----- notification start --------------------------------------------------")
+        dump(pizzaOrder)
+        print("----- notification end ----------------------------------------------------")
+    }
 }
 
 pizzaOrderPublisher.map { notification in
@@ -17,7 +22,16 @@ pizzaOrderPublisher.map { notification in
 .sink { orderStatus in
     print("Order status [\(orderStatus)]")
 }
+// 설계 종료 -----------------------------------------------------------------------------
 
+pizzaOrderPublisher.compactMap { notification in
+    notification.userInfo?["status"] as? OrderStatus
+}
+.assign(to: \.status, on: pizzaOrder) // 위의 status가 있으면, pizzaOrder의 status로 할당
+
+print("Order: \(pizzaOrder.status)") // 아직까지는 초기값 출력: 값을 추가하지 않았으니까
+
+// pizzaOrderPublisher 게시 -------------------------------------------------------------
 NotificationCenter.default.post(name: .didUpdateOrderStatus,
                                 object: pizzaOrder,
                                 userInfo: ["status": OrderStatus.processing])
