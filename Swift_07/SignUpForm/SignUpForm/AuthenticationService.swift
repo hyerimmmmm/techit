41d6686f0fa564605cf7dfc26042c7d982850c75
hyerim <ax1021@naver.com>
2024-06-19 09:54:00 +0900

    SignUpForm 파일에 AuthenticationService 추가

---
+++ Swift_07/SignUpForm/SignUpForm/AuthenticationService.swift
@@ -0,0 +1,53 @@
+//
+//  AuthenticationService.swift
+//  SignUpForm
+//
+//  Created by 김혜림 on 6/19/24.
+//
+
+import Foundation
+
+struct UserNameAvailableMessage: Codable {
+    var isAvailable: Bool
+    var userName: String
+}
+
+enum NetworkError: Error {
+    case transportError(Error)
+    case serverError(ststusCode: Int)
+    case noData
+    case decodingError(Error)
+    case encodingError(Error)
+}
+
+class AuthenticationService {
+    func checkUserNameAvailableWithClosure(userName: String,complection: @escaping (Result<Bool, NetworkError>) -> Void) {
+        let url = URL(string: "http://127.0.0.1:8080/isUserNameAvailable?userName=\(userName)")!
+        
+        let task = URLSession.shared.dataTask(with: url) { data, responese, error in
+            if let error = error {
+                complection(.failure(.transportError(error)))
+                return
+                }
+            if let responese = responese as? HTTPURLResponse, !(200..<300).contains(responese.statusCode) {
+                complection(.failure(.serverError(ststusCode: responese.statusCode)))
+                return
+            }
+            guard let data = data else {
+                complection(.failure(.noData))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let userAvailableMessage = try decoder.decode(UserNameAvailableMessage.self, from: data)
+                
+                complection(.success(userAvailableMessage.isAvailable))
+            } catch {
+                complection(.failure(.decodingError(error)))
+            }
+        }
+        
+        task.resume()
+    }
+}
--- Swift_07/SignUpForm/SignUpForm/ContentView.swift
+++ Swift_07/SignUpForm/SignUpForm/ContentView.swift
@@ -6,16 +6,98 @@
 //
 
 import SwiftUI
+import Combine
+
+class SignUpFormViewModel: ObservableObject {
+    @Published var username: String = ""
+    @Published var password: String = ""
+    @Published var passwordConfirmation: String = ""
+    
+    @Published var usernameMessage: String = ""
+    @Published var passwordMessage: String = ""
+    @Published var isValid: Bool = false
+    
+    private lazy var isUsernameLengthValidPublisher: AnyPublisher<Bool, Never> = {
+        $username.map { $0.count >= 3 }.eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordEmptyPublisher: AnyPublisher<Bool, Never> = {
+        $password.map(\.isEmpty).eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordMatchingPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest($password, $passwordConfirmation)
+            .map(==) // .map { $0 == $1 } 과 같은 뜻
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isPasswordValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { !$0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    private lazy var isFormValidPublisher: AnyPublisher<Bool, Never> = {
+        Publishers.CombineLatest(isUsernameLengthValidPublisher, isPasswordValidPublisher)
+            .map { $0 && $1 }
+            .eraseToAnyPublisher()
+    }()
+    
+    
+    
+    init() {
+        isFormValidPublisher.assign(to: &$isValid)
+        
+        // 3글자 이상 입력하지 않았을 경우 경고 문구 활성화
+        isUsernameLengthValidPublisher.map { $0 ? "" : "Username must be at least three chracters!" }
+            .assign(to: &$usernameMessage)
+        
+        Publishers.CombineLatest(isPasswordEmptyPublisher, isPasswordMatchingPublisher)
+            .map { isPasswordEmpty, isPasswordMatching in
+                if isPasswordEmpty {
+                    return "Password must be empty"
+                }
+                else if !isPasswordMatching {
+                    return "Passwords do mot match"
+                }
+                return ""
+            }
+            .assign(to: &$passwordMessage)
+    }
+}
 
 struct ContentView: View {
+    @StateObject var viewModel = SignUpFormViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Form {
+            // username
+            Section {
+                TextField("Username", text: $viewModel.username)
+                    .textInputAutocapitalization(.none)
+                    .autocorrectionDisabled()
+            } footer: {
+                Text(viewModel.usernameMessage)
+                    .foregroundStyle(Color.red)
+            }
+            
+            // password
+            Section {
+                SecureField("Password", text: $viewModel.password)
+                SecureField("Repeat password", text: $viewModel.passwordConfirmation)
+            } footer: {
+                Text(viewModel.passwordMessage)
+            }
+            .foregroundColor(.red)
+            
+            // submit button
+            Section {
+                Button("Sign up") {
+                    print("Signing up as \(viewModel.username)")
+                }
+                .disabled(!viewModel.isValid)
+            }
         }
-        .padding()
     }
 }
 
