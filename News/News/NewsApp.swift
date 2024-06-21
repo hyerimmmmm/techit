69f0153de86feb9bf91c83d24554ef5a8f5aa9f3
hyerim <ax1021@naver.com>
2024-06-21 15:31:39 +0900

    [News] News 파일 생성 / Config.xcconfig  파일 생성 / Project Info 의 Configurations 에서 환경변수 Debug,Release 에 Config 파일 선택(깃이그노어 같은 역할) / Info.plist 파일에 CLIENT_ID, CLIENT_SECRET 등록

---
+++ News/News/NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp.swift
+//  News
+//
+//  Created by 김혜림 on 6/21/24.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
