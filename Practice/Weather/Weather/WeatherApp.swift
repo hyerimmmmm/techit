7f8a8f05e3a51c4d411c5f9c3b819ceeb2ec26ab
hyerim <ax1021@naver.com>
2024-06-27 13:49:21 +0900

    실습 파일 추가

---
+++ Practice/Weather/Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 김혜림 on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
