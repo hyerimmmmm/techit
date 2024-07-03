52ca064a1c1a537e063e1b31a371eec764fedc43
hyerim <ax1021@naver.com>
2024-07-03 11:48:40 +0900

    [SearchApp] 검색어 제안 추가

--- Swift_08/SearchApp/SearchApp/ContentView.swift
+++ Swift_08/SearchApp/SearchApp/ContentView.swift
@@ -10,13 +10,21 @@ import SwiftUI
 struct ContentView: View {
     @State var searchText = ""
     
-    let petArray = ["Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog"]
+    let petArray = ["Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog", "Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog", "Cat", "Dog", "Fish", "Donkey", "Canary", "Camel", "Frog"]
     
     var body: some View {
         NavigationStack {
             PetListView(animals: petArray)
+                .navigationTitle("SearchApp")
         }
-        .searchable(text: $searchText) {
+        .navigationBarTitleDisplayMode(.automatic)
+        .searchable(text: $searchText, placement: .navigationBarDrawer(displayMode: .always), prompt: "Look for a pet") {
+            Text("Singing").searchCompletion("Canary")
+            Text("Croaking").searchCompletion("Frog")
+            Text("Grumpy").searchCompletion("Cat")
+            
+            Divider()
+            
             ForEach(petArray.filter { $0.hasPrefix(searchText) }, id: \.self) { name in
                 Text(name)
             }
@@ -33,6 +41,8 @@ struct PetListView: View {
     var body: some View {
         List(animals, id: \.self) { animal in
             Text(animal)
+                .background(Color.yellow)
         }
+        .listStyle(.plain)
     }
 }
