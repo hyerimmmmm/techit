729f7ffa418fc5d6c3ffcc8e225da8474deb1cf5
hyerim <ax1021@naver.com>
2024-07-04 10:29:12 +0900

    [MapApp] 지도 추가 / 위치정보 추가 / Annotation 추가

--- Swift_08/MapApp/MapApp/ContentView.swift
+++ Swift_08/MapApp/MapApp/ContentView.swift
@@ -6,16 +6,38 @@
 //
 
 import SwiftUI
+import MapKit
+
+extension CLLocationCoordinate2D {
+    static let paris = CLLocationCoordinate2D(latitude: 48.856613, longitude: 2.352222)
+}
 
 struct ContentView: View {
+    let locationManager = CLLocationManager()
+    
+    @State var message = "Map of Paris"
+    @State private var position: MapCameraPosition = .region(MKCoordinateRegion(center: .paris, span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)))
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Map(position: $position) {
+                Annotation("에펠탑", coordinate: .paris) {
+                    Image(systemName: "mappin.circle.fill")
+                        .foregroundColor(.purple)
+                        .background(.white)
+                        .clipShape(Circle())
+                }
+            }
+            
+            TextEditor(text: $message)
+                .frame(width: .infinity, height: 100)
         }
-        .padding()
+        .onAppear {
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.distanceFilter = kCLDistanceFilterNone
+            locationManager.requestWhenInUseAuthorization()
+            locationManager.startUpdatingLocation()
+        }
     }
 }
 
