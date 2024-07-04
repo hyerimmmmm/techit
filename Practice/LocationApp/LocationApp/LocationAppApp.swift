72d68c574950abac5b9ca317fefd09226396c321
hyerim <ax1021@naver.com>
2024-07-04 16:03:45 +0900

    [practice] LocationApp 파일 생성

---
+++ Practice/LocationApp/LocationApp/LocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAppApp.swift
+//  LocationApp
+//
+//  Created by 김혜림 on 7/4/24.
+//
+
+import SwiftUI
+
+@main
+struct LocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
