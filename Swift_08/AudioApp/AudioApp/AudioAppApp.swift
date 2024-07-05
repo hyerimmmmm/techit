41df91756e4468c2efea816a3c89c2db25edc5e1
hyerim <ax1021@naver.com>
2024-07-05 10:06:38 +0900

    [Swift_08] AudioApp 파일 생성

---
+++ Swift_08/AudioApp/AudioApp/AudioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioAppApp.swift
+//  AudioApp
+//
+//  Created by 김혜림 on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
