4dc606a746bf2045a846fc33bf63e658ac459b60
hyerim <ax1021@naver.com>
2024-07-22 13:40:52 +0900

    [Swift_11] fetchData 와 list 출력

--- Swift_11/NoteApp/NoteApp/ContentView.swift
+++ Swift_11/NoteApp/NoteApp/ContentView.swift
@@ -8,14 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showSheet = false
+    @State private var postDetent = PresentationDetent.medium
+    @StateObject private var viewModel = NoteViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(viewModel.notes, id: \.id) { note in
+                    VStack(alignment: .leading) {
+                        Text(note.title ?? "")
+                            .font(.system(size: 22, weight: .regular))
+                    }
+                }
+            }
+            .onAppear {
+                viewModel.fetchData()
+            }
+            .navigationTitle("Notes")
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Text(" X notes")
+                    Spacer()
+                    Button {
+                        showSheet.toggle()
+                    } label: {
+                        Image(systemName: "square.and.pencil")
+                    }
+                    .imageScale(.large)
+                    .sheet(isPresented: $showSheet) {
+                        FormView().presentationDetents([.large, .medium])
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
--- Swift_11/NoteApp/NoteApp/NoteViewModel.swift
+++ Swift_11/NoteApp/NoteApp/NoteViewModel.swift
@@ -12,4 +12,22 @@ class NoteViewModel: ObservableObject {
     @Published var notes = [Note]()
     
     private var databaseReference = Firestore.firestore().collection("Notes")
+    
+    func addData(title: String) {
+        let docRef = databaseReference.addDocument(data: ["title": title])
+        dump(docRef)
+    }
+    
+    func fetchData() {
+        databaseReference.addSnapshotListener { (QuerySnapshot, error) in
+            guard let documents = QuerySnapshot?.documents else {
+                print("No documents")
+                return
+            }
+            
+            self.notes = documents.compactMap { docSnap -> Note? in
+                return try? docSnap.data(as: Note.self)
+            }
+        }
+    }
 }
